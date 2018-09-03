32595724dea5032a0adf870a2f6e6cb88a7b500c
Hashir Umar <32617270+Hashir-Umar@users.noreply.github.com>
2018-09-04 01:39:43 +0500

    updating js file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $( document ).ready(function() {
 
+    function myFunction(x) {
+
+        if (x.matches) {
+            $('nav').css("padding", ".7rem");
+            $('.navbar-brand').css("font-size", "1.5em");
+            $('#header').css("height", "500px");
+        }
+        else {
+            $('nav').css("padding", "2rem 1rem");
+            $('nav .navbar-brand').css("font-size", "1.7em");
+            $('#header').css("height", "700px");
+        }
+    }
+
+
+    let x = window.matchMedia("(max-width: 500px)");
+    myFunction(x);
+    x.addListener(myFunction)
+
     setTimeout( function() {
 
         $('nav').css("padding", "30px 10px");
@@ -17,18 +36,27 @@ $( document ).ready(function() {
         $('#header').css("height", "700px");
         $('#header').css("opacity", "1");
 
+        let x = window.matchMedia("(max-width: 500px)");
+        myFunction(x);
+
       }, 900);
 
     $(window).scroll(function() {
 
+        let x = window.matchMedia("(max-width: 500px)");
+        myFunction(x);
+
         if ($(this).scrollTop() > 30) {
             $('.fixed-top').css("opacity", ".8");
-            $('nav').css("padding", ".5rem");
+            $('nav').css("padding", ".7rem");
             $('.navbar-brand').css("font-size", "1.5em");
         } else {
             $('.fixed-top').css("opacity", "1");
             $('nav').css("padding", "2rem 1rem");
             $('nav .navbar-brand').css("font-size", "1.7em");
+            x = window.matchMedia("(max-width: 500px)");
+            myFunction(x);
+
         }
 
         for (let index = 1; index <= 4; index++) {
@@ -39,19 +67,16 @@ $( document ).ready(function() {
 
     });
 
-    $('.carousel').carousel({
-      interval: 2000
-    })
 
-    $( "#work img" ).mouseover( function() {
-            $( '.mycard .details' ).css( "opacity", "1");
-            $( '.mycard' ).css( "height", "100%");
+    $( "#work .card-body" ).mouseover( function() {
+            $( '.overlay .btn' ).css( "opacity", "1");
+            $( '.overlay' ).css( "height", "100%");
         }
     );
 
-    $( ".mycard" ).mouseleave( function() {
-            $( '.mycard' ).css( "height", "0");
-            $( '.mycard .details' ).css( "opacity", "0");
+    $( ".overlay" ).mouseleave( function() {
+            $( '.overlay .btn' ).css( "opacity", "0");
+            $( '.overlay' ).css( "height", "0");
         }
     );
 
